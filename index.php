1c2116ed26048d9186426ae18342042ccbf068dd
Vitaliy Nikiforenko <v.nikiforenko.96@gmail.com>
2023-11-30 13:45:57 +0200

    update version

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 Plugin Name:  All posts ajax
 Plugin URI:
 Description:  All posts ajax load more, search and filter
-Version:      1.1.7
+Version:      1.1.8
 Author: WebRevizor
 Author URI: https://github.com/web-revizor/ajax-load-more-and-filters
 License:      GPL2
